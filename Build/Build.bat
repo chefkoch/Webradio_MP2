80c4426e10b85ae45a1c2a74b8bbfc11df2fb3e1
chefkoch <chefkoch@team-mediaportal.com>
2013-09-18 02:22:42 +0200

    Moved MP2 source root definition into it's own property file. This can be easily overwritten by defining a custom MP2.user.props file like described in the file comment

---
+++ Build/Build.bat
@@ -0,0 +1,2 @@
+
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Webradio.sln /target:Rebuild /property:Configuration=Release;Platform=x86 /l:FileLogger,Microsoft.Build.Engine;logfile=Build.log
