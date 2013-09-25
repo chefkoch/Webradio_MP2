3e2f0df7087cf2b02240f83ebdc24fc7fd16017c
chefkoch <chefkoch@team-mediaportal.com>
2013-09-25 03:35:36 +0200

    updated targets in case csproj is built directly

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,2 +1,2 @@
 
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Webradio.sln /target:Rebuild /property:Configuration=Release;Platform=x86 /l:FileLogger,Microsoft.Build.Engine;logfile=Build.log
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Webradio.csproj /target:Rebuild /property:Configuration=Release;Platform=x86;SolutionName=Webradio /l:FileLogger,Microsoft.Build.Engine;logfile=Build.log
